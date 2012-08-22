0e368b1877547c18b834116ec26d8f67bd46290b
Stefan Schlott <stefan@ploing.de>
2012-08-22 20:07:14 +0200

    Fixed md-xml parsing. Code formatting.

--- src/main/scala/de/ploing/slideasm/SlideAsm.scala
+++ src/main/scala/de/ploing/slideasm/SlideAsm.scala
@@ -12,7 +12,7 @@ import org.pegdown.Extensions
 object SlideAsm {
   def main(args: Array[String]): Unit = {
     println("SlideAsm - the html5 slide assembler")
-    if (args.length!=1) {
+    if (args.length != 1) {
       println("Exactly one parameter (assembly file) required")
       System.exit(1)
     }
@@ -25,33 +25,32 @@ object SlideAsm {
     config.load(scala.io.Source.fromFile(mainFile), true)
 
     val slideBaseDir = mainFile.getParent
-    val templateName = Paths.get(mainFile.getParent(), config.get("main","template").get, "template.html")
+    val templateName = Paths.get(mainFile.getParent(), config.get("main", "template").get, "template.html")
     val template = XML.loadFile(templateName.toString)
-    
-    val sections = config.get("main","sections")
+
+    val sections = config.get("main", "sections")
     if (!sections.isDefined) {
       println("sections in main missing")
       System.exit(1)
     }
     sections.get.split(",").foreach(section => {
-    	println(section)
-    	val slides = config.get(section, "slides")
-    	if (slides.isDefined) {
-    	  slides.get.split(",").foreach(slideDir => {
-    	    println("  " + slideDir)
-    	    val slideMdFile = Paths.get(slideBaseDir.toString, slideDir, "slide.md")
-    	    val slideHtmlFile = Paths.get(slideBaseDir.toString, slideDir, "slide.html")
-    	    val slide = if (Files.isReadable(slideMdFile)) {
-    	      val mdParser = new PegDownProcessor(Extensions.ALL)
-    	      val mdString = new String(Files.readAllBytes(slideMdFile))
-    	      val parsedMd = "<article>" + mdParser.markdownToHtml(mdString) + "</article>" 
-    	      println(parsedMd)
-    	      XML.load(parsedMd)    	      
-    	    } else { 
-    	      XML.loadFile(slideHtmlFile.toString) 
-    	    }
-    	  } )
-    	}
-    } )
+      println(section)
+      val slides = config.get(section, "slides")
+      if (slides.isDefined) {
+        slides.get.split(",").foreach(slideDir => {
+          println("  " + slideDir)
+          val slideMdFile = Paths.get(slideBaseDir.toString, slideDir, "slide.md")
+          val slideHtmlFile = Paths.get(slideBaseDir.toString, slideDir, "slide.html")
+          val slide = if (Files.isReadable(slideMdFile)) {
+            val mdParser = new PegDownProcessor(Extensions.ALL)
+            val mdString = new String(Files.readAllBytes(slideMdFile))
+            val parsedMd = "<article>" + mdParser.markdownToHtml(mdString) + "</article>"
+            XML.loadString(parsedMd)
+          } else {
+            XML.loadFile(slideHtmlFile.toString)
+          }
+        })
+      }
+    })
   }
 }
